2b2d2a1122fd37c18a8e20c6eb280bb907e39c4a
Shaheen Gandhi <shaheen@oculus.com>
2020-02-17 10:51:09 -0800

    [rust-api] Add rust API over FFI for imtui-ncurses

---
+++ imtui-examples/examples/ncurses0.rs
@@ -0,0 +1,45 @@
+use imtui;
+use imgui;
+use std::time::SystemTime;
+use std;
+
+fn main() {
+    let mut imgui = imgui::Context::create();
+    imgui.set_ini_filename(None);
+
+    let mut imtui = imtui::Ncurses::init(false, 60.0, -1.0);
+    let now = SystemTime::now();
+    let mut nframes = 0 as i32;
+    let mut fval = 123.0;
+
+    loop {
+
+        nframes += 1;
+        imtui.set_active();
+        imtui.new_frame();
+        let ui = imgui.frame();
+        let title = imgui::ImString::new("Hello, world!");
+        let window = imgui::Window::new(&title)
+            .position([0.0, 0.0], imgui::Condition::Always)
+            .size([50.0, 10.0], imgui::Condition::Always);
+        if let Some(windowToken) = window.begin(&ui) {
+            ui.text(format!("NFrames = {}", nframes));
+
+            let imgui_io = ui.io();
+            ui.text(format!("Mouse Post: x = {}, y = {}", imgui_io.mouse_pos[0], imgui_io.mouse_pos[1]));
+            ui.text(format!("Time per frame {0:.3} ms/frame ({1:.1} FPS)", 1000.0 / imgui_io.framerate, imgui_io.framerate));
+
+            ui.text("Float:");
+            ui.same_line(0.0);
+
+            let range = std::ops::RangeInclusive::new(0.0, 1000.0);
+            let label = imgui::ImString::new("##float");
+            let slider_builder = imgui::Slider::new(&label, range);
+            slider_builder.build(&ui, &mut fval);
+            windowToken.end(&ui);
+        }
+
+        let draw_data = ui.render();
+        imtui.render(draw_data);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,58 @@
+use imgui;
+use imgui::internal::{RawCast};
+use std;
+
 pub use imtui_sys::root as sys;
+
+pub struct Ncurses {
+    screen: *mut sys::ImTui::TScreen,
+    is_active: bool,
+}
+
+impl Ncurses {
+    pub fn init(mouse_support: bool, active_fps: f32, idle_fps: f32) -> Ncurses {
+        let screen: *mut sys::ImTui::TScreen;
+        unsafe {
+            screen = sys::ImTui_ImplNcurses_Init(mouse_support, active_fps, idle_fps);
+            sys::ImTui_ImplText_Init();
+        }
+        Ncurses {
+            screen: screen,
+            is_active: false,
+        }
+    }
+
+    pub fn set_active(&mut self) {
+        self.is_active = true;
+    }
+
+    pub fn set_inactive(&mut self) {
+        self.is_active = false;
+    }
+
+    pub fn new_frame(&self) -> bool {
+        let input_pending: bool;
+        unsafe {
+            input_pending = sys::ImTui_ImplNcurses_NewFrame();
+            sys::ImTui_ImplText_NewFrame();
+        }
+        input_pending
+    }
+
+    pub fn render(&self, draw_data: &imgui::DrawData) {
+        unsafe {
+            let raw_ptr = draw_data.raw() as *const imgui::sys::ImDrawData as *mut imgui::sys::ImDrawData;
+            sys::ImTui_ImplText_RenderDrawData(raw_ptr, self.screen);
+            sys::ImTui_ImplNcurses_DrawScreen(self.is_active);
+        }
+    }
+
+    pub fn drop(&mut self) {
+        unsafe {
+            sys::ImTui_ImplText_Shutdown();
+            sys::ImTui_ImplNcurses_Shutdown();
+        }
+        self.screen = std::ptr::null_mut();
+        self.is_active = false;
+    }
+}
