b39b1d4da56cf768d97b3ea394e9949d5829eb9a
Shaheen Gandhi <shaheen@fb.com>
2020-09-13 15:59:36 -0700

    Use async update of state

--- imtui-examples/examples/hnterm.rs
+++ imtui-examples/examples/hnterm.rs
@@ -12,6 +12,13 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use chrono::{DateTime, Utc};
 use std::ops::Add;
+use reqwest;
+use std::error;
+use futures::task::LocalSpawnExt;
+use futures::executor::LocalPool;
+use std::sync::Mutex;
+use std::cell::Cell;
+use std::rc::Rc;
 
 mod hn;
 
@@ -141,16 +148,44 @@ enum HnItem {
 }
 
 struct HnState {
-    items: HashMap<HnItemId, HnItem>,
     top_ids: Vec<HnItemId>,
     show_ids: Vec<HnItemId>,
     ask_ids: Vec<HnItemId>,
     new_ids: Vec<HnItemId>,
+    changed_ids: Vec<HnItemId>,
+}
+
+impl HnState {
+
+    async fn fetch_ids(url: reqwest::Url) -> Result<Vec<HnItemId>, reqwest::Error> {
+        reqwest::get(url)
+            .await?
+            .json::<Vec<HnItemId>>()
+            .await
+    }
+
+    async fn fetch() -> Result<HnState, Box<dyn error::Error>> {
+        let base_url = reqwest::Url::parse("https://hacker-news.firebaseio.com/v0").unwrap();
+        let top_ids = HnState::fetch_ids(base_url.join("topstories.json").unwrap()).await?;
+        let show_ids = HnState::fetch_ids(base_url.join("showstories.json").unwrap()).await?;
+        let ask_ids = HnState::fetch_ids(base_url.join("askstories.json").unwrap()).await?;
+        let new_ids = HnState::fetch_ids(base_url.join("newstories.json").unwrap()).await?;
+        let changed_ids = HnState::fetch_ids(base_url.join("updates.json").unwrap()).await?;
+
+        let state = HnState {
+            top_ids,
+            show_ids,
+            ask_ids,
+            new_ids,
+            changed_ids,
+        };
+        Ok(state)
+    }
 }
 
 struct AppState {
     windows: Vec<WindowData>,
-    hn_state: Option<HnState>,
+    hn_state: Rc<Cell<Option<HnState>>>,
     last_update_time: SystemTime,
     next_update: SystemTime,
 }
@@ -159,6 +194,7 @@ struct HntermApp {
     imgui: imgui::Context,
     imtui: imtui::Ncurses,
     state: AppState,
+    executor: LocalPool,
 }
 
 impl AppState {
@@ -169,7 +205,7 @@ impl AppState {
                 WindowData::new(WindowContent::Top),
                 WindowData::new(WindowContent::Top),
             ],
-            hn_state: None,
+            hn_state: Rc::new(Cell::new(None)),
             last_update_time: SystemTime::UNIX_EPOCH,
             next_update: SystemTime::now(),
         }
@@ -183,15 +219,21 @@ impl AppState {
         !ui.is_key_pressed('q' as u32)
     }
 
-    fn update(&mut self) -> bool {
+    fn update(&mut self, spawner: &impl LocalSpawnExt) {
         let now = SystemTime::now();
         if now.duration_since(self.last_update_time).unwrap() < Duration::new(30, 0) {
-            return false;
+            return;
         }
+
+        let state_ref = Rc::clone(&self.hn_state);
+
+        let fetch_and_assign = async move {
+            state_ref.set(Some(HnState::fetch().await.unwrap()));
+        };
+        spawner.spawn_local(fetch_and_assign).unwrap();
 
         self.last_update_time = now;
         self.next_update = now.add(Duration::new(30, 0));
-        true
     }
 }
 
@@ -200,12 +242,13 @@ impl HntermApp {
         HntermApp {
             imgui,
             imtui,
+            executor: LocalPool::new(),
             state: AppState::new(),
         }
     }
 
     fn process_frame(&mut self) -> bool {
-        self.state.update();
+        self.state.update(&self.executor.spawner());
 
         for (i, wd) in self.state.windows.iter_mut().enumerate() {
             wd.title = format!(
